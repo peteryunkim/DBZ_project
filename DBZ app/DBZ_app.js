fa39264ae426e9b28deeac18e69cfc9bf2b00a62
Naz Khan <nazmuskhan@yahoo.com>
2017-05-06 15:35:42 -0400

    JS changes

--- DBZ app/DBZ_app.js
+++ DBZ app/DBZ_app.js
@@ -13,20 +13,21 @@ function done(){
     url: "http://localhost:3000/characters",
     success: function(data){
       data.forEach(function(char){
-        $('.name').append(`<div class="row center">
-        <div class="col s6">
+        debugger
+        $('.name').append(`<div class="row right-align">
+
           <div class="card center">
             <div class="card-image">
-              <img src="http://icons.iconarchive.com/icons/musett/dragon-ballz/256/Dragon-Ball-icon.png">
-              <span class="card-title">${char.name}</span>
+              <img src=${char.img} height="60%" width="30">
+              <span class="card-title text orange accent-4 ">${char.name}</span>
             </div>
             <div class="card-content">
             </div>
             <div class="card-action">
-            <button type="button" class="btn" data-id="${char.id}" id="info">More Info</button>
+            <button type="button" class="btn amber darken-1" data-id="${char.id}" id="info">More Info</button>
             </div>
           </div>
-        </div>
+
       </div>
       `)
       })
@@ -50,7 +51,7 @@ function moreInfo(){
             <li>${data.DOB}</li>
             <li>${data.special_techniques}</li>
           </ul>
-          <a href="" src="location.reload()">Back</a>
+          <a href="" src="done()">Back</a>
           `)
       }
     })
@@ -67,6 +68,8 @@ function createFighter(){
     const $power_level = $('#power_level').val()
     const $DOB = $('#DOB').val()
     const $special_techniques = $('#special_techniques').val()
+    const $img = $('#img').val()
+
     // debugger
     $.ajax({
       method: 'post',
@@ -76,13 +79,15 @@ function createFighter(){
         race: $race,
         power_level: $power_level,
         DOB: $DOB,
-        special_techniques: $special_techniques
+        special_techniques: $special_techniques,
+        img: $img
+
         }
       },
       success: function(data){
-        $('.name').append(`<p>${data.name}</p><button type="button" class="btn" data-id="${data.id}" id="info">More Info</button>`)
+        $('.name').append(`<p>${data.name}</p><button type="button" class="btn amber darken-1" data-id="${data.id}" id="info">More Info</button>`)
       }
-    })
+    }).then(moreInfo)
   })
 
 }
